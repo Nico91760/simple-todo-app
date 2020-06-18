6886b072e630da49f4586140b48bc77994848afa
Nicolas Palamara <nicopalam@hotmail.fr>
2020-06-18 17:06:10 +0100

    Updates

---
+++ precache-manifest.8bf7d76782df77fe45b3a0297c86217d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69efb4991c32ff005c395ab4e173d7f9",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "993e01df92b834026638",
+    "url": "/simple-todo-app/static/css/main.fc373e81.chunk.css"
+  },
+  {
+    "revision": "e1c993e3070ff6d419bc",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993e01df92b834026638",
+    "url": "/simple-todo-app/static/js/main.d4fe513a.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
